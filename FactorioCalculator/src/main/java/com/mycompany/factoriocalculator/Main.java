62c67c342f19e508452fc9cf05aa13519aca0237
Matthew <Matthew@DESKTOP-O9K2L6L.lan>
2021-03-06 15:08:32 -0500

    Added SpringApplication imports to main class

--- FactorioCalculator/src/main/java/com/mycompany/factoriocalculator/Main.java
+++ FactorioCalculator/src/main/java/com/mycompany/factoriocalculator/Main.java
@@ -7,11 +7,15 @@ package com.mycompany.factoriocalculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  *
  * @author Matthew
  */
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Main {
 
     /**
